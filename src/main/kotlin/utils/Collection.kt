cb5dba7684c1052847a011d2f3baa42e165e8084
lilsher <artem.sher21.11@mail.ru>
2023-04-07 21:34:40 +0300

    collection ConsoleManager FileManager

--- src/main/kotlin/utils/Collection.kt
+++ src/main/kotlin/utils/Collection.kt
@@ -1,4 +1,31 @@
 package utils
-
+import com.google.gson.Gson
+import java.io.File
+import java.nio.charset.Charset
+import java.util.*
 class Collection {
+
+    // Класс для парсинга JSON-файла
+    data class Item(val name: String, val value: Int)
+
+    fun main() {
+        val queue = PriorityQueue<Int>()
+
+        val file = File("values.json")
+        if (file.exists() && file.isFile) {
+            val json = file.readText(Charset.defaultCharset())
+            val items = Gson().fromJson(json, Array<Item>::class.java)
+
+            items.forEach {
+                queue.offer(it.value)
+            }
+        } else {
+            println("Файл values.json не найден!")
+        }
+
+        println("Элементы PriorityQueue:")
+        while (queue.isNotEmpty()) {
+            println(queue.poll())
+        }
+    }
 }
--- src/main/kotlin/utils/FileManager.kt
+++ src/main/kotlin/utils/FileManager.kt
@@ -1,4 +1,38 @@
 package utils
 
+import java.nio.charset.StandardCharsets
+import java.nio.file.Files
+import java.nio.file.Path
+import java.nio.file.Paths
+import java.io.File
+import java.nio.charset.StandardCharsets
+import com.google.gson.Gson
+
+/**
+
+*/
 class FileManager {
-}
+
+
+    // Записывает JSON-строку в файл на пути path
+    fun writeJsonToFile(path: String, json: String) {
+        val file = File(path)
+        if (!file.exists()) {
+            file.createNewFile()
+        }
+        file.writeText(json, StandardCharsets.UTF_8)
+    }
+
+    // Читает JSON-строку из файла на пути path и конвертирует ее в объект типа T
+    fun readJsonFromFile(path: String): T {
+        val file = File(path)
+        if (!file.exists()) {
+            throw FileException("Не получается открыть файл")
+        }
+        val json = file.readText(StandardCharsets.UTF_8)
+        return Gson().fromJson(json, T::class.java)
+    }
+    /**
+    path предоставляет путь к файлу
+     */
+    }
